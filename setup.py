e796c0033df5ab8ddd50ee9a1acd87b49c57022c
John David Pressman <jd@jdpressman.com>
2019-01-12 22:37:47 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mdx-wl-import",
+    version="0.2",
+    install_requires=['Markdown>=2.6.11'],
+    author="John David Pressman",
+    author_email="jd@jdpressman.com",
+    description=("Whistling Lobsters custom include extension. Lets you take "
+                 "data from a url and include it in the rendered markdown."),
+    license="MIT",
+    url="https://github.com/JD-P/mdx_wl_import")
+    
